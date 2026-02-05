c00dbf45ecfd95606e5460787a71ca4a7e0ffa70
a78335323-hash <a78335323@gmail.com>
2026-02-05 09:14:31 +0100

    Update route.ts

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,1 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const res = NextResponse.redirect(new URL("/login", req.url), 302);
+  res.cookies.set("dash_session", "", {
+    path: "/",
+    maxAge: 0,
+  });
+  return res;
+}
 
