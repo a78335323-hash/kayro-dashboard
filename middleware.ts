1fb74499d3c324241ef78f7d87dc673aec891a10
a78335323-hash <a78335323@gmail.com>
2026-02-10 09:52:15 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,3 +4,14 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   return NextResponse.next();
 }
+if (
+  pathname.startsWith("/api") ||
+  pathname.startsWith("/login") ||
+  pathname.startsWith("/debug") ||
+  pathname.startsWith("/_next") ||
+  pathname.startsWith("/favicon.ico") ||
+  pathname.startsWith("/login-1") ||
+  pathname.startsWith("/login-2")
+) {
+  return NextResponse.next();
+}
