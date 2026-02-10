bfc0d601edc1dcb97c4438d700462e8a3ce88593
a78335323-hash <a78335323@gmail.com>
2026-02-10 09:33:32 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,31 +2,5 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-
-  // ✅ lascia passare TUTTE le API (test-db incluso)
-  if (
-    pathname.startsWith("/api") ||
-    pathname.startsWith("/login") ||
-    pathname.startsWith("/_next") ||
-    pathname.startsWith("/favicon.ico") ||
-    pathname.startsWith("/login-1") ||
-    pathname.startsWith("/login-2")
-  ) {
-    return NextResponse.next();
-  }
-
-  const session = req.cookies.get("dash_session")?.value;
-
-  if (session !== "1") {
-    const url = req.nextUrl.clone();
-    url.pathname = "/login";
-    return NextResponse.redirect(url);
-  }
-
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/((?!_next/static|_next/image).*)"],
-};
